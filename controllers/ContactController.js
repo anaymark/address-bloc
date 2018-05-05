fc6c475d24bab1b5684fd08cc8c44b82a9729cc8
alexander sydorenko <dangerzone@alexanders-MacBook-Pro.local>
2018-05-05 14:41:52 -0600

    Add contact search/delete functionality

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,4 +1,3 @@
-const inquirer = require("inquirer");
 const Contact = require("../db/models").Contact;
 
 module.exports = class ContactController{
@@ -30,9 +29,93 @@ module.exports = class ContactController{
 			}
 		}
 		];
+
+		this.searchQuestions = [
+		{
+			type:"input",
+			name:"name",
+			message:"Name of contact to search - ",
+			validate(val){
+				return val !== "";
+			}
+		}
+		];
+
+		this.showContactQuestions = [
+        {
+        	type: "list",
+        	name: "selected",
+        	message: "Please choose from an option below: ",
+       	    choices: [
+          		"Delete contact",
+          		"Main menu"
+        	]
+        }
+        ];
+
+        this.deleteConfirmQuestions = [
+        {
+        	type: "confirm",
+        	name: "confirmation",
+        	message: "are you sure you want to delete this contact?"
+        }
+        ];
+
 	}
 
 	addContact(name,phone,email){
 		return Contact.create({name,phone,email})
 	}
+
+	getContacts(){
+		return Contact.findAll();
+	}
+
+	iterativeSearch(contacts,target){
+		for(let contact of contacts){
+			if(contact.name.toLowerCase() === target.toLowerCase()){
+				return contact;
+			}
+		}
+		return null;
+	}
+
+	binarySearch(contacts,target){
+		let min = 0;
+		let max = contacts.length - 1;
+		let mid;
+
+		while(min <= max){
+			mid = Math.floor((min+max)/2);
+			let currentContact = contacts[mid];
+
+			if(currentContact.name > target)
+			{
+				max = mid - 1;
+			}
+			else if(currentContact.name < target){
+				min = mid + 1;
+			}
+			else{
+				return contacts[mid];
+			}
+		}
+		return null;
+	}
+
+	search(name){
+	  return Contact.findOne({
+	  	where: {name}
+	  });
+	}
+
+	delete(id){
+	  return Contact.destroy({
+		where:{id}
+	  })
+	}
 }
+
+
+
+
